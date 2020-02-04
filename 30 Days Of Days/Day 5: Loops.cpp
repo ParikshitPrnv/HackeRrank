67f1b5fdce2020c177c9f221294370c5e161ef26
Pranav Gautam <33193926+ParikshitPrnv@users.noreply.github.com>
2020-02-05 00:02:03 +0530

    Update Day 5: Loops.cpp

--- 30 Days Of Days/Day 5: Loops.cpp
+++ 30 Days Of Days/Day 5: Loops.cpp
@@ -7,13 +7,15 @@ Each multiple n*i (where 1<=i<=10) should be printed on a new line in the form: 
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 int main()
 {
     int n;
     cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
+    //cin.ignore(numeric_limits<streamsize>::max(), '\n');
+for(int i=1;i<=10;i++)
+{
+    cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+}
     return 0;
 }
+
