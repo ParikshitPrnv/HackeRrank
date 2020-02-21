6a5a60e81e0bb26092211b7dfae6942dd7653ecc
Pranav Gautam <33193926+ParikshitPrnv@users.noreply.github.com>
2020-02-21 21:57:24 +0530

    Create Day 9: Recursion 3.cpp

---
+++ 30 Days of Code/Day 9: Recursion 3.cpp
@@ -0,0 +1,32 @@
+/*Write a factorial function that takes a positive integer,N  as a parameter and prints the result of N! (N factorial).*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the factorial function below.
+int factorial(int n) {
+int fact=1;
+    for(int i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+return fact;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
