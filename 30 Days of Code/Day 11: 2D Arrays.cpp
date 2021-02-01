cc2b737db99c3569f7a593825fec72976ab5db90
Pranav Gautam <33193926+ParikshitPrnv@users.noreply.github.com>
2021-02-01 13:04:01 +0530

    Create Day 11: 2D Arrays.cpp

---
+++ 30 Days of Code/Day 11: 2D Arrays.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int a[6][6];
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    int big=INT_MIN;
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            
+            if(j+2<6 && i+2<6)
+            {
+                int sum=a[i][j]+a[i][j+1]+a[i][j+2]+a[i+1][j+1]+a[i+2][j]+a[i+2][j+1]+a[i+2][j+2];
+                 
+                if(sum>big)
+            big=sum;
+            }
+           
+            
+        }
+    }
+cout<<big;
+    return 0;
+}
